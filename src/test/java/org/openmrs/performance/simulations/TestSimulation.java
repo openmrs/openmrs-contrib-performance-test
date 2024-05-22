c772a9d23181b8fa9d0618f55bfaddd71c6b19e7
Jayasanka Weerasinghe <33048395+jayasanka-sack@users.noreply.github.com>
2024-05-22 17:27:26 +0530

    Closed modal 250

--- src/test/java/org/openmrs/performance/simulations/TestSimulation.java
+++ src/test/java/org/openmrs/performance/simulations/TestSimulation.java
@@ -25,8 +25,8 @@ public class TestSimulation extends Simulation {
 	{
 		setUp(
 				clerkScenario.injectClosed(
-						rampConcurrentUsers(0).to(300).during(20),
-						constantConcurrentUsers(300).during(60)
+						rampConcurrentUsers(0).to(250).during(20),
+						constantConcurrentUsers(250).during(60)
 				),
 				doctorScenario.injectOpen(
 						rampUsers(5).during(10)
