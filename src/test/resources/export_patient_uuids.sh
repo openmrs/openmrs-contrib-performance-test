7f9c69bc76d5988e0c48260497cc93a2a7c1ae91
Jayasanka Weerasinghe <33048395+jayasanka-sack@users.noreply.github.com>
2024-06-28 16:57:18 +0100

    Add more patients

--- src/test/resources/export_patient_uuids.sh
+++ src/test/resources/export_patient_uuids.sh
@@ -8,10 +8,10 @@ DB_PORT="3306"
 DB_HOST="127.0.0.1"
 
 # Query to be executed
-QUERY="SELECT uuid FROM patient INNER JOIN person ON patient.patient_id = person.person_id;"
+QUERY="SELECT uuid AS patient_uuid FROM patient INNER JOIN person ON patient.patient_id = person.person_id;"
 
 # Output CSV file
-OUTPUT_FILE="src/test/resources/patient_uuids.csv"
+OUTPUT_FILE="patient_uuids.csv"
 
 mysql -u$DB_USER -p$DB_PASS -D$DB_NAME -P$DB_PORT -h$DB_HOST -e "$QUERY" | sed 's/\t/,/g' > $OUTPUT_FILE
 
