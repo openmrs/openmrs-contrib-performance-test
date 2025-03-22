e67e76e8137600033a9340419a06ae5d6f5f930a
Bawantha Thilan <38850236+Bawanthathilan@users.noreply.github.com>
2025-03-23 00:46:28 +0530

    O3-4554: Configure automatic code formatting (#88)
    * config revelc maven formatter plugin and add new formatted files
    
    * change java vertion 15 to 17

--- src/test/java/org/openmrs/performance/utils/CommonUtils.java
+++ src/test/java/org/openmrs/performance/utils/CommonUtils.java
@@ -10,25 +10,23 @@ import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 public class CommonUtils {
-	
+
 	public static List<String> extractConceptIds(String response) {
 		List<String> conceptIds = new ArrayList<>();
-		
+
 		try {
 			ObjectMapper objectMapper = new ObjectMapper();
 			JsonNode jsonObject = objectMapper.readTree(response);
 			JsonNode entries = jsonObject.get("entry");
 
 			if (entries != null && entries.isArray()) {
-				return StreamSupport.stream(entries.spliterator(), false)
-						.map(entry -> entry.get("resource"))
-						.filter(Objects::nonNull)
-						.map(resource -> resource.path("code").path("coding"))
-						.filter(coding -> coding.isArray() && !coding.isEmpty())
-						.map(coding -> coding.get(0).get("code").asText())
-						.collect(Collectors.toList());
+				return StreamSupport.stream(entries.spliterator(), false).map(entry -> entry.get("resource"))
+				        .filter(Objects::nonNull).map(resource -> resource.path("code").path("coding"))
+				        .filter(coding -> coding.isArray() && !coding.isEmpty())
+				        .map(coding -> coding.get(0).get("code").asText()).collect(Collectors.toList());
 			}
-		} catch (Exception e) {
+		}
+		catch (Exception e) {
 			throw new RuntimeException(e);
 		}
 
