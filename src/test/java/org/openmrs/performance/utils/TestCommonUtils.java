ec421df4afe1c8f6d4aae735d04594c18ef58849
ganeshdevisetti <jayendraganesh21@gmail.com>
2025-04-04 01:10:13 +0530

    O3-4592: Adding date util function

--- src/test/java/org/openmrs/performance/utils/TestCommonUtils.java
+++ src/test/java/org/openmrs/performance/utils/TestCommonUtils.java
@@ -2,9 +2,13 @@ package org.openmrs.performance.utils;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestCommonUtils {
 
@@ -43,5 +47,23 @@ public class TestCommonUtils {
 		List<String> expected = List.of("123", "456");
 
 		assertEquals(CommonUtils.extractConceptIds(response), expected);
+	}
+
+	@Test
+	public void testGetCurrentDateTimeAsString_Format() {
+		String currentDateTime = CommonUtils.getCurrentDateTimeAsString();
+		System.out.println(currentDateTime);
+		assertTrue(currentDateTime.matches("^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{6}(Z|[+-]\\d{2}:\\d{2})$"),
+		    "DateTime format is incorrect");
+	}
+
+	@Test
+	public void testGetAdjustedDateTimeAsString_PositiveAdjustment() {
+		int daysToAdjust = 5;
+		String adjustedDateTimeStr = CommonUtils.getAdjustedDateTimeAsString(daysToAdjust);
+		ZonedDateTime adjustedDateTime = ZonedDateTime.parse(adjustedDateTimeStr, DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+		ZonedDateTime expectedDateTime = ZonedDateTime.now().plusDays(daysToAdjust);
+		assertEquals(0, Math.abs(ChronoUnit.SECONDS.between(adjustedDateTime, expectedDateTime)),
+		    "Date adjustment is incorrect");
 	}
 }
