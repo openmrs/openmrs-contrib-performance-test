619b579165352c59917a55a540b170eadbc5184e
dilankavishka <dkgamage01@gmail.com>
2025-08-03 18:47:32 +0530

    Add Form Builder persona, scenario, registry, and HTTP service for performance testing

---
+++ src/test/java/org/openmrs/performance/personas/FormBuilderPersona.java
@@ -0,0 +1,21 @@
+package org.openmrs.performance.personas;
+
+import org.openmrs.performance.registries.FormBuilderRegistry;
+import org.openmrs.performance.registries.LabTechRegistry;
+import org.openmrs.performance.scenarios.OpenFormBuilderTabScenario;
+import org.openmrs.performance.scenarios.PatientLabScenario;
+import org.openmrs.performance.scenarios.Scenario;
+
+import java.util.List;
+
+public class FormBuilderPersona extends Persona<FormBuilderRegistry> {
+
+	public FormBuilderPersona(double loadShare) {
+		super(loadShare);
+	}
+
+	@Override
+	public List<Scenario<FormBuilderRegistry>> getScenarios() {
+		return List.of(new OpenFormBuilderTabScenario(1));
+	}
+}
